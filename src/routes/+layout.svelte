7392be093fa968925549251232498ee5e4b6aff5
Gio <giorgi.kumelashvili21@gmail.com>
2025-01-15 23:07:09 +0400

    added sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,68 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
+  import * as Breadcrumb from "$lib/components/ui/breadcrumb";
+  import { Separator } from "$lib/components/ui/separator";
+  import * as Sidebar from "$lib/components/ui/sidebar";
+  import { toggleMode } from "mode-watcher";
+  import { ModeWatcher } from "mode-watcher";
+
+  import Sun from "lucide-svelte/icons/sun";
+  import Moon from "lucide-svelte/icons/moon";
+  import { Button } from "$lib/components/ui/button";
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<Sidebar.Provider>
+  <AppSidebar />
+
+  <Sidebar.Inset>
+    <header
+      class="flex h-16 w-full shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+    >
+      <div class="flex flex-1 items-center gap-2 px-4">
+        <Sidebar.Trigger class="-ml-1" />
+
+        <Separator orientation="vertical" class="mr-2 h-4" />
+
+        <Breadcrumb.Root>
+          <Breadcrumb.List>
+            <Breadcrumb.Item class="hidden md:block">
+              <Breadcrumb.Link href="#">
+                Building Your Application
+              </Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator class="hidden md:block" />
+            <Breadcrumb.Item>
+              <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+            </Breadcrumb.Item>
+          </Breadcrumb.List>
+        </Breadcrumb.Root>
+
+        <Button
+          onclick={() => toggleMode()}
+          variant="outline"
+          size="icon"
+          class="ml-auto h-7 w-7"
+        >
+          <Sun
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Moon
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+      </div>
+    </header>
+
+    <!-- Root body -->
+    <div class="p-4 pt-0 h-full overflow-y-auto">
+      {@render children()}
+    </div>
+  </Sidebar.Inset>
+</Sidebar.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,73 @@
 <script lang="ts">
   import Button from "$lib/components/ui/button/button.svelte";
+  import TestDialog from "./test-dialog.svelte";
+  import { buttonVariants } from "$lib/components/ui/button";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 
   let greetMsg = $state("");
 </script>
 
-<main>
-  <h1>{greetMsg}</h1>
-  <Button onclick={() => (greetMsg = "Hello button clicked")}>Hello</Button>
-</main>
+<h1>{greetMsg}</h1>
+<Button onclick={() => (greetMsg = "Hello button clicked")}>Hello</Button>
+<TestDialog />
+<h1>Hello</h1>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger class={buttonVariants({ variant: "outline" })}>
+    Open
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content class="w-56">
+    <DropdownMenu.Group>
+      <DropdownMenu.GroupHeading>My Account</DropdownMenu.GroupHeading>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Group>
+        <DropdownMenu.Item>
+          Profile
+          <DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Billing
+          <DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Settings
+          <DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Keyboard shortcuts
+          <DropdownMenu.Shortcut>⌘K</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Group>
+        <DropdownMenu.Item>Team</DropdownMenu.Item>
+        <DropdownMenu.Sub>
+          <DropdownMenu.SubTrigger>Invite users</DropdownMenu.SubTrigger>
+          <DropdownMenu.SubContent>
+            <DropdownMenu.Item>Email</DropdownMenu.Item>
+            <DropdownMenu.Item>Message</DropdownMenu.Item>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>More...</DropdownMenu.Item>
+          </DropdownMenu.SubContent>
+        </DropdownMenu.Sub>
+        <DropdownMenu.Item>
+          New Team
+          <DropdownMenu.Shortcut>⌘+T</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>GitHub</DropdownMenu.Item>
+      <DropdownMenu.Item>Support</DropdownMenu.Item>
+      <DropdownMenu.Item>API</DropdownMenu.Item>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>
+        Log out
+        <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
+      </DropdownMenu.Item>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
+
+{#each { length: 60 }}
+  <p>Hello</p>
+{/each}
